0952fd4e38409a91700348ab20ae47729b30e2d0
Angel Soto <angelsoto9597@gmail.com>
2023-04-30 12:48:00 -0400

    here is my rodwave-auth completed

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://angelsoto9597:demo@clusterrodwave.dcgzrbg.mongodb.net/rodwave?retryWrites=true&w=majority',
+    'dbName': 'rodwave'
+};
